664222779354d6d51dda2bee450bb3929e5bdb57
Kirill Malev <playittodeath@gmail.com>
2021-01-09 21:52:28 +0300

    actually sending message back some time later

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ And after that resends messages some time later.
 """
 
 import logging
-import time
+import asyncio
 
 
 from aiogram import Bot, Dispatcher, executor, types
@@ -24,6 +24,7 @@ logging.basicConfig(level=logging.INFO)
 # Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+loop = asyncio.get_event_loop()
 
 
 @dp.message_handler(commands=['start', 'help'])
@@ -34,20 +35,26 @@ async def send_welcome(message: types.Message):
     await message.reply("Hi!\nI'm Spaced repetition bot!\nPowered by aiogram.")
 
 
-@dp.message_handler(content_types=[types.ContentType.ANY])
-async def echo_handler(message: types.Message):
-    """
-    Handler will forward received message back to the sender
-    """
-    await bot.forward_message(
-        from_chat_id=message.chat.id, chat_id=message.chat.id, message_id=message.message_id
-    )
+async def spacely_repeat(message: types.Message):
+    """Spacely repeats the messages."""
     for time_interval in TIME_INTERVALS:
-        time.sleep(time_interval)
+        await asyncio.sleep(time_interval)
         await bot.forward_message(
             from_chat_id=message.chat.id, chat_id=message.chat.id, message_id=message.message_id
         )
 
 
+@dp.message_handler(content_types=[types.ContentType.ANY])
+async def echo_handler(message: types.Message):
+    """
+    Handler will forward received message back to the sender
+    """
+    print(message)
+    await bot.forward_message(
+        from_chat_id=message.chat.id, chat_id=message.chat.id, message_id=message.message_id
+    )
+    loop.create_task(spacely_repeat(message))
+
+
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=False)
